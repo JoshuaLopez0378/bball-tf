a9cf3a06159faacc211f8fe8dbb954e23e5b0217
JoshuaLopez0378 <joshuaalbertlopez0378@gmail.com>
2024-04-17 23:19:09 +0800

    feat: add show list of current games to user

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import json
-from scripts.bball_requests import date_yesterday, call_games
+from scripts.bball_requests import date_yesterday, check_games
 from scripts.node_requests import compare_team_stats, simulate_node_request
 
 print("Start")
 ### For test: 04/04/2024 <-- GSW has no C
 string_date = date_yesterday(3)
-stats_list = call_games(string_date)
-compared_list = compare_team_stats(stats_list)
-simulate_node_request(compared_list)
+all_games_list, request_games_data = check_games(string_date)
+
+print("Choose game, enter ID")
+for game in all_games_list:
+    print(game)
+choice = input("Choice: ")
+
+
+
+
+# compared_list = compare_team_stats(stats_list)
+# simulate_node_request(compared_list)
 
 print("End")
--- scripts/bball_requests.py
+++ scripts/bball_requests.py
@@ -13,75 +13,85 @@ def date_yesterday(days=1):
     string_date = yester_date.strftime('%Y-%m-%d')
     return string_date
 
-def call_games(date_yesterday): 
+def check_games(date_yesterday): 
+    # Request prerequisites
     today_date_url_param = f'?dates[]={date_yesterday}'
     headers = {
         "accept":"application/json",
         "Authorization":API_KEY
     }
-    games = requests.get(url=games_url+today_date_url_param, headers=headers)
-    
-    print("=== GAMES RESULT ===")
-    games_result = games.json()
-    games_data = games_result["data"][0]
+
+    # Perform get request and take result
+    request_games = requests.get(url=f"{games_url}{today_date_url_param}", headers=headers)
+    print("=== ALL GAMES RESULT ===")
+    request_games_result = request_games.json()
+    request_games_data = request_games_result["data"]
     # print(games_data)
 
+    # List all games, "home vs away" format
+    all_games_list = [f"{res[0] + 1} | {res[1]['home_team']['full_name']} VS {res[1]['visitor_team']['full_name']}" for res in enumerate(request_games_data)]
+    print(all_games_list)
+    # for games in all_games_list:
+        # print(games)
+    
+    return all_games_list, request_games_data
 
-    home_team_name = games_data["home_team"]["full_name"]
-    home_team_score = games_data["home_team_score"]
-    home_team_id = games_data["home_team"]["id"]
-    print(f"HOME: {home_team_name}: {home_team_score} | (id:{home_team_id})")
 
-    visitor_team_name = games_data["visitor_team"]["full_name"]
-    visitor_team_score = games_data["visitor_team_score"]
-    visitor_team_id = games_data["visitor_team"]["id"]
-    print(f"AWAY: {visitor_team_name}: {visitor_team_score} | (id:{visitor_team_id})")
+    # home_team_name = games_data["home_team"]["full_name"]
+    # home_team_score = games_data["home_team_score"]
+    # home_team_id = games_data["home_team"]["id"]
+    # print(f"HOME: {home_team_name}: {home_team_score} | (id:{home_team_id})")
+
+    # visitor_team_name = games_data["visitor_team"]["full_name"]
+    # visitor_team_score = games_data["visitor_team_score"]
+    # visitor_team_id = games_data["visitor_team"]["id"]
+    # print(f"AWAY: {visitor_team_name}: {visitor_team_score} | (id:{visitor_team_id})")
 
     
-    # Put in another function next time
-    # STATS
-    today_game_id = games_data["id"]
-    game_id_url_param = f'?game_ids[]={today_game_id}'
+    # # Put in another function next time
+    # # STATS
+    # today_game_id = games_data["id"]
+    # game_id_url_param = f'?game_ids[]={today_game_id}'
 
-    stats = requests.get(url=stats_url + game_id_url_param , headers=headers)
-    stats_result = stats.json()
-    next_cursor = str(stats_result["meta"]["next_cursor"])
-    stats2 = requests.get(url=stats_url + game_id_url_param + '&cursor=' + next_cursor, headers=headers)
-    stats_result2 = stats2.json()
-
-
-    print("==============================================================================")
-    pd.set_option('display.max_colwidth', 100)
-
-    df_stats_pg1 = pd.json_normalize(stats_result['data'],meta=['id'])
-    df_stats_pg2 = pd.json_normalize(stats_result2['data'],meta=['id'])
-    df_all_stats = pd.concat([df_stats_pg1, df_stats_pg2], ignore_index=True)
-
-    df_take_cols = df_all_stats[['player.jersey_number', 'player.first_name', 'player.last_name', 'player.position', 'pts', 'team.id', 'team.full_name', 'game.id']]
-    df_arranged = df_take_cols.sort_values(by=['team.full_name', 'player.position', 'pts'], ascending=False, ignore_index=True)
-    df_arranged["player.position"] = df_arranged['player.position'].apply(use_prime_position)
+    # stats = requests.get(url=stats_url + game_id_url_param , headers=headers)
+    # stats_result = stats.json()
+    # next_cursor = str(stats_result["meta"]["next_cursor"])
+    # stats2 = requests.get(url=stats_url + game_id_url_param + '&cursor=' + next_cursor, headers=headers)
+    # stats_result2 = stats2.json()
 
 
-    df_center_home = df_arranged.loc[(df_arranged['player.position'] == 'C') & (df_arranged['team.id'] == home_team_id)].sort_values(by=['pts'], ascending=False).head(1)
-    df_center_visitor = df_arranged.loc[(df_arranged['player.position'] == 'C') & (df_arranged['team.id'] == visitor_team_id)].sort_values(by=['pts'], ascending=False).head(1)
+    # print("==============================================================================")
+    # pd.set_option('display.max_colwidth', 100)
 
-    forward_count_home = forward_count_visitor = 2
-    print("=== ===")
-    print(df_center_home.empty)
-    if df_center_home.empty:
-        forward_count_home = 3
-    elif df_center_visitor.empty:
-        forward_count_visitor = 3
+    # df_stats_pg1 = pd.json_normalize(stats_result['data'],meta=['id'])
+    # df_stats_pg2 = pd.json_normalize(stats_result2['data'],meta=['id'])
+    # df_all_stats = pd.concat([df_stats_pg1, df_stats_pg2], ignore_index=True)
 
-    df_forward_home = df_arranged.loc[(df_arranged['player.position'] == 'F') & (df_arranged['team.id'] == home_team_id)].sort_values(by=['pts'], ascending=False).head(forward_count_home)
-    df_forward_visitor = df_arranged.loc[(df_arranged['player.position'] == 'F') & (df_arranged['team.id'] == visitor_team_id)].sort_values(by=['pts'], ascending=False).head(forward_count_visitor)
+    # df_take_cols = df_all_stats[['player.jersey_number', 'player.first_name', 'player.last_name', 'player.position', 'pts', 'team.id', 'team.full_name', 'game.id']]
+    # df_arranged = df_take_cols.sort_values(by=['team.full_name', 'player.position', 'pts'], ascending=False, ignore_index=True)
+    # df_arranged["player.position"] = df_arranged['player.position'].apply(use_prime_position)
 
-    df_guard_home = df_arranged.loc[(df_arranged['player.position'] == 'G') & (df_arranged['team.id'] == home_team_id)].sort_values(by=['pts'], ascending=False).head(2)
-    df_guard_visitor = df_arranged.loc[(df_arranged['player.position'] == 'G') & (df_arranged['team.id'] == visitor_team_id)].sort_values(by=['pts'], ascending=False).head(2)    
 
-    home_df = pd.concat([df_center_home, df_forward_home, df_guard_home], ignore_index=True)
-    visitor_df = pd.concat([df_center_visitor, df_forward_visitor, df_guard_visitor], ignore_index=True)
+    # df_center_home = df_arranged.loc[(df_arranged['player.position'] == 'C') & (df_arranged['team.id'] == home_team_id)].sort_values(by=['pts'], ascending=False).head(1)
+    # df_center_visitor = df_arranged.loc[(df_arranged['player.position'] == 'C') & (df_arranged['team.id'] == visitor_team_id)].sort_values(by=['pts'], ascending=False).head(1)
 
-    print(home_df, "\n", visitor_df)
+    # forward_count_home = forward_count_visitor = 2
+    # print("=== ===")
+    # print(df_center_home.empty)
+    # if df_center_home.empty:
+    #     forward_count_home = 3
+    # elif df_center_visitor.empty:
+    #     forward_count_visitor = 3
 
-    return [home_df, visitor_df]
+    # df_forward_home = df_arranged.loc[(df_arranged['player.position'] == 'F') & (df_arranged['team.id'] == home_team_id)].sort_values(by=['pts'], ascending=False).head(forward_count_home)
+    # df_forward_visitor = df_arranged.loc[(df_arranged['player.position'] == 'F') & (df_arranged['team.id'] == visitor_team_id)].sort_values(by=['pts'], ascending=False).head(forward_count_visitor)
+
+    # df_guard_home = df_arranged.loc[(df_arranged['player.position'] == 'G') & (df_arranged['team.id'] == home_team_id)].sort_values(by=['pts'], ascending=False).head(2)
+    # df_guard_visitor = df_arranged.loc[(df_arranged['player.position'] == 'G') & (df_arranged['team.id'] == visitor_team_id)].sort_values(by=['pts'], ascending=False).head(2)    
+
+    # home_df = pd.concat([df_center_home, df_forward_home, df_guard_home], ignore_index=True)
+    # visitor_df = pd.concat([df_center_visitor, df_forward_visitor, df_guard_visitor], ignore_index=True)
+
+    # print(home_df, "\n", visitor_df)
+
+    # return [home_df, visitor_df]
